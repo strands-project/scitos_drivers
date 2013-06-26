e2c5c82206ed3c475a7577182b09ffbb63bc28cb
Chris Burbridge <cburbridge@gmail.com>
2013-06-26 17:30:09 +0100

    config file as ros parameter

--- src/scitos_driver/src/scitos_node.cpp
+++ src/scitos_driver/src/scitos_node.cpp
@@ -3,20 +3,43 @@
 #include <scitos_driver/ScitosG5.h>
 
 #include <boost/thread.hpp>
-
+#include <string>
+#include <vector>
 int main(int argc, char **argv) {
-	mira::Framework framework(argc, argv, true);
+	ros::init(argc, argv, "scitos_ros");
+	ros::NodeHandle node;
+	std::string  config_file, port_number;
+	std::vector<std::string> args;
 
-	//	printf("waiting for param interface...\n");
-	//#	robot_->getMiraAuthority().waitForServiceInterface("IRobotModelProvider");
-	//#	printf("done.\n");
+	if (argc < 2) { // no arguments, so use ROS parameters.
 
+	  if (node.getParam("config_file", config_file))  {
+		args.push_back(std::string("-c"));
+		args.push_back(config_file);
+	  } else {
+		ROS_ERROR("Can't read parameter 'config_file'");
+		return 1;
+	  }
+	  if (node.getParam("server_port", port_number))  {
+		args.push_back(std::string("-p"));
+		args.push_back(port_number);
+	  } else {
+		ROS_INFO("Not loading with MIRA multiprocess support.");
+	  }
+	} else {
+	  for (int i=1; i<argc;i++)
+		args.push_back(std::string(argv[i]));
+	}
+	
+	mira::Framework framework(args, true);
+	
 	while (!framework.isInExec()) {
 	  boost::this_thread::sleep(boost::posix_time::milliseconds(50));
 	}
+	// above is no use.
+	boost::this_thread::sleep(boost::posix_time::milliseconds(5000));
 
-
-	ros::init(argc, argv, "scitos_ros");
+	
 
 	ROS_INFO("Creating G5...");
 	ScitosG5 s;
