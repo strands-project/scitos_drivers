d5d422c76ca28bd563c82927e967716dc6bcfab0
Chris Burbridge <cburbridge@gmail.com>
2013-06-27 08:56:13 +0100

    fixed parameter acces for private namespace.

--- src/scitos_driver/src/scitos_node.cpp
+++ src/scitos_driver/src/scitos_node.cpp
@@ -6,21 +6,21 @@
 #include <string>
 #include <vector>
 int main(int argc, char **argv) {
-	ros::init(argc, argv, "scitos_ros");
-	ros::NodeHandle node;
+	ros::init(argc, argv, "scitos_node");
+
 	std::string  config_file, port_number;
 	std::vector<std::string> args;
 
 	if (argc < 2) { // no arguments, so use ROS parameters.
 
-	  if (node.getParam("config_file", config_file))  {
+	  if (ros::param::get("~config_file", config_file))  {
 		args.push_back(std::string("-c"));
 		args.push_back(config_file);
 	  } else {
 		ROS_ERROR("Can't read parameter 'config_file'");
 		return 1;
 	  }
-	  if (node.getParam("server_port", port_number))  {
+	  if (ros::param::get("~server_port", port_number))  {
 		args.push_back(std::string("-p"));
 		args.push_back(port_number);
 	  } else {
