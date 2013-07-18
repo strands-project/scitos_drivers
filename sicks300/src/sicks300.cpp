058bdc9b4cecc11708b8a6bfb77bcf423d297232
Chris Burbridge <cburbridge@gmail.com>
2013-07-18 16:53:43 +0100

    Removed unused code; added ability to select field of view.

--- sicks300/src/sicks300.cpp
+++ sicks300/src/sicks300.cpp
@@ -34,6 +34,9 @@
  *  Copyright (C) 2000
  *     Brian Gerkey, Kasper Stoy, Richard Vaughan, & Andrew Howard
  *
+ *
+ * Adaptations:
+ *  - remoted tf publication, made field of view controllable.
  */
 
 #include "sicks300.h"
@@ -46,62 +49,27 @@ SickS300::SickS300()
   ros::NodeHandle param_node("~");
   ros::NodeHandle nodeHandle("/");
 
-  int param;
-  double x, y, z;
-
   // reading transformation parameters from parameter server
   param_node.param(std::string("frame"), scan_data_.header.frame_id, std::string("base_laser_link"));
-  param_node.param(std::string("send_transform"), param, 1);
-  if (param)
-  {
-    send_transform_ = true;
-  }
-  else
-  {
-    send_transform_ = false;
-  }
-  param_node.param(std::string("tf_x"), x, 0.115);
-  param_node.param(std::string("tf_y"), y, 0.0);
-  param_node.param(std::string("tf_z"), z, 0.21);
+ 
+  // Reduce field of view to this number of degrees
+  double fov;
+  param_node.param(std::string("field_of_view"), fov, 270.0);
+  field_of_view_ = (int)(fov*2.0); // angle increment is .5 degrees
+  field_of_view_ <<=1; field_of_view_ >>=1; // round to a multiple of two
+  start_scan_ = 270 - field_of_view_/2;
+  end_scan_ = 270 + field_of_view_/2;
 
-  transform_vector_ = tf::Vector3(x, y, z);
+  scan_data_.angle_min = -(field_of_view_/4.0) / 180.f * M_PI;
+  scan_data_.angle_max = -(field_of_view_/4.0) / 180.f * M_PI;
+  scan_data_.angle_increment = 0.5f / 180.f * M_PI;
+  scan_data_.time_increment = 0;
+  scan_data_.scan_time = 0.08;
+  scan_data_.range_min = 0.1;
+  scan_data_.range_max = 29.0;
+  scan_data_.ranges.resize(field_of_view_);
+  scan_data_.intensities.resize(field_of_view_);
 
-  // Setting full field of view (270 degree) or reduced (180 degree)
-  param_node.param(std::string("reduced_fov"), param, 0);
-  if (param != 0)
-  {
-    reduced_FOV_ = true;
-    ROS_INFO("INFO: Starting Sick300-Laser with reduced field ov view of 180 degree");
-  }
-  else
-  {
-    reduced_FOV_ = false;
-  }
-
-  if (!reduced_FOV_)
-  {
-    scan_data_.angle_min = -135.f / 180.f * M_PI;
-    scan_data_.angle_max = 135.f / 180.f * M_PI;
-    scan_data_.angle_increment = 0.5f / 180.f * M_PI;
-    scan_data_.time_increment = 0;
-    scan_data_.scan_time = 0.08;
-    scan_data_.range_min = 0.1;
-    scan_data_.range_max = 29.0;
-    scan_data_.ranges.resize(541);
-    scan_data_.intensities.resize(541);
-  }
-  else
-  {
-    scan_data_.angle_min = -90.f / 180.f * M_PI;
-    scan_data_.angle_max = 90.f / 180.f * M_PI;
-    scan_data_.angle_increment = 0.5f / 180.f * M_PI;
-    scan_data_.time_increment = 0;
-    scan_data_.scan_time = 0.08;
-    scan_data_.range_min = 0.1;
-    scan_data_.range_max = 29.0;
-    scan_data_.ranges.resize(361);
-    scan_data_.intensities.resize(361);
-  }
 
   // Reading device parameters
   param_node.param(std::string("devicename"), device_name_, std::string("/dev/sick300"));
@@ -109,7 +77,7 @@ SickS300::SickS300()
 
   connected_ = serial_comm_.connect(device_name_, baud_rate_);
 
-  scan_data_publisher_ = nodeHandle.advertise<sensor_msgs::LaserScan> ("laserscan", 10);
+  scan_data_publisher_ = nodeHandle.advertise<sensor_msgs::LaserScan> ("scan", 10);
 
 }
 
@@ -130,31 +98,14 @@ void SickS300::update()
 
     float* ranges = serial_comm_.getRanges();
     unsigned int numRanges = serial_comm_.getNumRanges();
-    if (!reduced_FOV_)
-    {
-      scan_data_.ranges.resize(numRanges);
-      for (unsigned int i = 0; i < numRanges; i++)
-        scan_data_.ranges[i] = ranges[i];
-    }
-    else
-    {
-      for (unsigned int i = 0; i < 361; i++)
-        scan_data_.ranges[i] = ranges[i + 89];
-    }
+
+    for (unsigned int i = start_scan_, j=0; i < end_scan_; i++, j++)
+      scan_data_.ranges[j] = ranges[i];
+
     scan_data_.header.stamp = ros::Time::now();
 
     scan_data_publisher_.publish(scan_data_);
 
-  }
-
-}
-
-void SickS300::broadcast_transform()
-{
-  if (send_transform_)
-  {
-    tf_broadcaster_.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion(0, 0, 0, 1), transform_vector_),
-                                                      ros::Time::now(), "base_link", "base_laser_link"));
   }
 
 }
@@ -176,7 +127,6 @@ int main(int argc, char** argv)
   {
 
     sickS300.update();
-    sickS300.broadcast_transform();
 
     ros::spinOnce();
     loop_rate.sleep();
