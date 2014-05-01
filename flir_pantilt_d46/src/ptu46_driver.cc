14043d510f2a41b01953c3c32b393c46ac29518b
Bob <strands@bob.(none)>
2014-05-01 15:04:56 +0100

    Revert "Merge pull request #54 from marc-hanheide/hydro-devel"
    This reverts commit 71e00ddcabb0ee9981d59033a2cb7b505db08ab9, reversing
    changes made to 51742257dd76556f461efb567828a7d5108bec48. The changes result in
    the /ptu/state topic being broken. Probably because of line 325 in
    flir_pantilt_d46/src/ptu46_driver.cc.

--- flir_pantilt_d46/src/ptu46_driver.cc
+++ flir_pantilt_d46/src/ptu46_driver.cc
@@ -319,14 +319,6 @@ float PTU46::GetPosition (char type) {
     return strtod (&buffer[2],NULL) * GetResolution(type);
 }
 
-void PTU46::SetCheckLimits(bool val) {
-    check_limits = val;
-    if (check_limits) {
-        Write("le ");
-    } else {
-        Write("ld ");
-    }
-}
 
 // set position in radians
 bool PTU46::SetPosition (char type, float pos, bool Block) {
@@ -337,11 +329,9 @@ bool PTU46::SetPosition (char type, float pos, bool Block) {
     int Count = static_cast<int> (pos/GetResolution(type));
 
     // Check limits
-    if (check_limits) {
-        if (Count < (type == PTU46_TILT ? TMin : PMin) || Count > (type == PTU46_TILT ? TMax : PMax)) {
-            fprintf (stderr,"Pan Tilt Value out of Range: %c %f(%d) (%d-%d)\n", type, pos, Count, (type == PTU46_TILT ? TMin : PMin),(type == PTU46_TILT ? TMax : PMax));
-            return false;
-        }   
+    if (Count < (type == PTU46_TILT ? TMin : PMin) || Count > (type == PTU46_TILT ? TMax : PMax)) {
+        fprintf (stderr,"Pan Tilt Value out of Range: %c %f(%d) (%d-%d)\n", type, pos, Count, (type == PTU46_TILT ? TMin : PMin),(type == PTU46_TILT ? TMax : PMax));
+        return false;
     }
 
     char cmd[16];
